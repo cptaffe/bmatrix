2a22e2306cd2751c3a64f2c346936c9a4cda92f4
Connor Taffe <cpaynetaffe@gmail.com>
2016-05-14 17:10:08 -0500

    Updated password Authentication
    Auth3PID & AuthUser now privide AuthenticationFoo routines which
    Reply3PID and ReplyUser call after verifying password strength

--- client/auth/password/password.go
+++ client/auth/password/password.go
@@ -1,15 +1,130 @@
 package password
 
-import zxcbvn "github.com/nbutton23/zxcvbn-go"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cptaffe/bmatrix/client/auth"
+	"github.com/cptaffe/bmatrix/client/errors"
+	zxcbvn "github.com/nbutton23/zxcvbn-go"
+)
+
+// Implements Matrix r0.1.0 §3.1.2.1
 
 var (
 	// Xkcd is the password entropy for the famous
 	// xkcd password 'correcthorsebatterystaple',
 	// it is given here as a reference for a good password
-	Xkcd = Entropy("correcthorsebatterystaple")
+	Xkcd = Password("correcthorsebatterystaple")
 )
 
+// Password represents a passsword
+type Password string
+
 // Entropy returns a password's entropy
-func Entropy(p string) float64 {
-	return zxcbvn.PasswordStrength(p, nil).Entropy
+func (p Password) Entropy() float64 {
+	return zxcbvn.PasswordStrength(string(p), nil).Entropy
+}
+
+// Handler implements auth.Handler
+type Handler struct {
+	AuthUser AuthUser
+	Auth3PID Auth3PID
+}
+
+// AuthUser authenticates a user reply
+type AuthUser interface {
+	AuthenticateUser(*ReplyUser) (bool, error)
+}
+
+// Auth3PID authenticates a 3pid reply
+type Auth3PID interface {
+	Authenticate3PID(*Reply3PID) (bool, error)
+}
+
+// HandleAuth handles parsing the authentication information into the
+// correct type and calling Authenticate
+// NOTE: on absence of corresponding Auth member, silently fails
+func (h *Handler) HandleAuth(a map[string]interface{}) (bool, error) {
+	b, err := json.Marshal(a)
+	if err != nil {
+		return false, errors.New(errors.BadJSON, err)
+	}
+	if a["user"] != nil {
+		// ReplyUser style reply
+		rp := &ReplyUser{}
+		if err := json.Unmarshal(b, rp); err != nil {
+			return false, errors.New(errors.BadJSON, err)
+		}
+		if h.AuthUser != nil {
+			return rp.Authenticate(h.AuthUser)
+		}
+		return false, nil
+	} else if a["medium"] != nil {
+		// medium must equal 'email'
+		// Reply3PID style reply
+		rp := &Reply3PID{}
+		if err := json.Unmarshal(b, rp); err != nil {
+			return false, errors.New(errors.BadJSON, err)
+		}
+		if h.Auth3PID != nil {
+			return rp.Authenticate(h.Auth3PID)
+		}
+		return false, nil
+	}
+	return false, errors.New(errors.UnknownToken, fmt.Errorf("Improper format for reply, see §3.1.2.1"))
+}
+
+func testPasswordEntropy(p Password) error {
+	if p.Entropy() < 0.8*Xkcd.Entropy() {
+		return errors.New(errors.WeakPassword, fmt.Errorf("Password is %fx < 0.8x the entropy of the reference password '%s'; see https://xkcd.com/936/", p.Entropy()/Xkcd.Entropy(), string(Xkcd)))
+	}
+	return nil
+}
+
+// ReplyUser is the Auth reply
+type ReplyUser struct {
+	Type     auth.Stage `json:"type"` // must be auth.PasswordStage
+	User     string     `json:"user"`
+	Password Password   `json:"password"`
+}
+
+// Authenticate authenticates a user
+func (r *ReplyUser) Authenticate(auth AuthUser) (bool, error) {
+	if err := testPasswordEntropy(r.Password); err != nil {
+		return false, err
+	}
+	// TODO: authenticate with credentials
+	return auth.AuthenticateUser(r)
+}
+
+// Medium represents possible mediums
+type Medium string
+
+const (
+	// EmailMedium is currently the only supported medium
+	EmailMedium Medium = "email"
+)
+
+// TODO: UnmarshalJSON check that it equals EmailMedium
+
+// Reply3PID is 3rd party identification
+type Reply3PID struct {
+	Type     auth.Stage `json:"type"`
+	Medium   Medium     `json:"medium"` // "has to be 'email'"
+	Address  string     `json:"address"`
+	Password Password   `json:"password"`
+}
+
+// Authenticate authenticates a user via 3pid
+func (r *Reply3PID) Authenticate(auth Auth3PID) (bool, error) {
+	// sanity check because we don't have special checks in parsing
+	if r.Medium != EmailMedium {
+		return false, errors.New(errors.UnknownToken, fmt.Errorf("3pid requests must have 'medium' as 'email'; see §3.1.2.1"))
+	}
+	if err := testPasswordEntropy(r.Password); err != nil {
+		return false, err
+	}
+	// TODO: authenticate with credentials
+	return auth.Authenticate3PID(r)
 }
--- client/auth/password/password_test.go
+++ client/auth/password/password_test.go
@@ -1,11 +1,69 @@
 package password
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestEntropy(t *testing.T) {
-	p := "correcthorsebatterystaple"
-	e := Entropy(p)
-	if e < 45 || e > 46 && e == Xkcd {
-		t.Errorf("Entropy for '%s' is ~45, but returned '%f'", p, e)
+	e := Xkcd.Entropy()
+	if e < 45 || e > 46 {
+		t.Errorf("Entropy for '%s' is ~45, but returned '%f'", string(Xkcd), e)
+	}
+}
+
+type testAuther struct {
+	Pass string
+}
+
+func (t *testAuther) Authenticate3PID(r *Reply3PID) (bool, error) {
+	return string(r.Password) == t.Pass, nil
+}
+
+func (t *testAuther) AuthenticateUser(r *ReplyUser) (bool, error) {
+	return string(r.Password) == t.Pass, nil
+}
+
+func TestHandleAuth(t *testing.T) {
+	ur := []byte(`{
+  "type": "m.login.password",
+  "user": "cptaffe",
+  "password": "abc"
+}`)
+	urm := map[string]interface{}{}
+	if err := json.Unmarshal(ur, &urm); err != nil {
+		t.Error(err)
+	}
+	ta := &testAuther{Pass: "abc"}
+	b, err := (&Handler{
+		Auth3PID: ta,
+		AuthUser: ta,
+	}).HandleAuth(urm)
+	t.Log(err)
+	if b != false || err == nil {
+		t.Errorf("Password should have been too weak")
+	}
+}
+
+func TestHandleAuth3PID(t *testing.T) {
+	ur := []byte(`{
+  "type": "m.login.password",
+  "user": "cptaffe",
+  "medium": "email",
+	"address": "cpaynetaffe@gmail.com",
+	"password": "zaba;"
+}`)
+	urm := map[string]interface{}{}
+	if err := json.Unmarshal(ur, &urm); err != nil {
+		t.Error(err)
+	}
+	ta := &testAuther{Pass: "abc"}
+	b, err := (&Handler{
+		Auth3PID: ta,
+		AuthUser: ta,
+	}).HandleAuth(urm)
+	t.Log(err)
+	if b != false || err == nil {
+		t.Errorf("Password should have been too weak")
 	}
 }
